49fa66d5f8574813662a4e8de7bb4ff48d56c701
Vadim <vadimpenchak@gmail.com>
2017-01-15 19:50:01 +0200

    Added my solution to Max Consecutive Ones problem.

---
+++ 485_Max_Consecutive_OnesTestHelper/TestCase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace _485_Max_Consecutive_OnesTestHelper
+{
+    public class TestCase
+    {
+        [XmlElement]
+        public int Expected { get; set; }
+
+        [XmlArrayItem("int")]
+        public int[] Numbers { get; set; }
+    }
+}
