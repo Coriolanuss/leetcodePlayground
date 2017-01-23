2f652108c2a4fe3e2e00213b47067118270ad0b4
Vadim <vadimpenchak@gmail.com>
2017-01-23 17:43:24 +0200

    Added qualitative comments and discriptions. Added two more tests for 'two islands' and 'inner lake' test cases.

--- 463_Island_Perimeter/IslandPerimeter_Solutions.cs
+++ 463_Island_Perimeter/IslandPerimeter_Solutions.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace _463_Island_Perimeter
 {
-    // TODO Needs description
+    /// <summary>
+    /// Contains solutions to the Island Perimeter problem
+    /// </summary>
     public class IslandPerimeter_Solutions
     {
-        // TODO Needs description
+        /// <summary>
+        /// My first solution based on simple looping through all 'land' positions and checking their neighbors.
+        /// Works fine with 'two (and more) islands' input
+        /// Alse works fine with 'inner lake' input
+        /// </summary>
+        /// <param name="grid">An Int32 array of ones and zeros with size less then 1000x1000. Where '1' is land and '0' is water</param>
+        /// <returns>Perimeter of island</returns>
         public static int IslandPerimeterUsingCycleAndNeighborCheck(int[,] grid)
         {
             int result = 0;
@@ -22,24 +30,24 @@ namespace _463_Island_Perimeter
             return result;
         }
 
-        // TODO Write a comment
         /// <summary>
-        /// 
+        /// Checks 'land' position's neighbors to be water. 
         /// </summary>
+        /// <returns>Number of 'shores' of one 'land' position</returns>
         private static int GetShoreCount(int[,] grid, int i, int j)
         {
             int shoreCount = 0;
 
-            // check left neighbor
+            // check west neighbor
             if (j == 0 || (j > 0 && grid[i, j - 1] == 0))
                 shoreCount++;
-            // check top neighbor
+            // check north neighbor
             if (i == 0 || (i > 0 && grid[i - 1, j] == 0))
                 shoreCount++;
-            // check right neighbor
+            // check east neighbor
             if (j == grid.GetLength(1) - 1 || (j < grid.GetLength(1) - 1 && grid[i, j + 1] == 0))
                 shoreCount++;
-            // check bottom neighbor
+            // check south neighbor
             if (i == grid.GetLength(0) - 1 || (i < grid.GetLength(0) -1 && grid[i + 1, j] == 0))
                 shoreCount++;
 
--- 463_Island_PerimeterTests/IslandPerimeter_SolutionsTests.cs
+++ 463_Island_PerimeterTests/IslandPerimeter_SolutionsTests.cs
@@ -68,7 +68,7 @@ namespace _463_Island_Perimeter.Tests
         }
 
         [TestMethod()]
-        public void IslandPerimeterUsingCycleAndNeighborCheckTest_TestCase3_Returns16()
+        public void IslandPerimeterUsingCycleAndNeighborCheckTest_TestCase4_Returns16()
         {
             // Arrange
             int[,] input = {
@@ -76,6 +76,42 @@ namespace _463_Island_Perimeter.Tests
                 {1, 1, 1, 1 },
                 {1, 1, 1, 1 },
                 {1, 1, 1, 1 }};
+            int expected = 16;
+
+            // Act
+            int actual = IslandPerimeter_Solutions.IslandPerimeterUsingCycleAndNeighborCheck(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void IslandPerimeterUsingCycleAndNeighborCheckTest_TestCase5TwoIslands_Returns16()
+        {
+            // Arrange
+            int[,] input = {
+                {1, 1, 0, 0 },
+                {1, 1, 0, 0 },
+                {0, 0, 0, 1 },
+                {0, 0, 1, 1 }};
+            int expected = 16;
+
+            // Act
+            int actual = IslandPerimeter_Solutions.IslandPerimeterUsingCycleAndNeighborCheck(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void IslandPerimeterUsingCycleAndNeighborCheckTest_TestCase6InnerLake_Returns16()
+        {
+            // Arrange
+            int[,] input = {
+                {1, 1, 1, 0 },
+                {1, 0, 1, 0 },
+                {1, 1, 1, 0 },
+                {0, 0, 0, 0 }};
             int expected = 16;
 
             // Act
