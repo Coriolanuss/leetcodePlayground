5ece84bab54fcfa82e4337cc7a6959e3a022abc8
Vadim <vadimpenchak@gmail.com>
2017-01-31 19:43:42 +0200

    Added my first try (and optimal) solution to the NimGame problem.

---
+++ 292_Nim_Game/NimGame_Soluions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _292_Nim_Game
+{
+    /// <summary>
+    /// Solutions to a Nim Game problem, where you and your opponent are given a heap of stones and both of you can take 1, 2 or 3 stones from the heap in turns. The one who takes the last stone wins.
+    /// The problem is to culculate if you can win at all in case of given number of stones "n" in the heap and it's your turn.
+    /// </summary>
+    public class NimGame_Soluions
+    {
+        /// <summary>
+        /// My first try to solve the problem without looking into any hints or references.
+        /// </summary>
+        /// <note>It turned out that this solution is best in terms math.</note>
+        /// <param name="n">Number of stones in the heap. Must be />0</param>
+        /// <returns>"True" if you can win and "false" if you can't at the given conditions</returns>
+        public static bool CanWinNimMyFirstSolution(int n)
+        {
+            if (n % 4 == 0)
+                return false;
+            else
+                return true;
+        }
+    }
+    // TODO Add Unit-tests
+}
--- 485_Max_Consecutive_Ones/MaxConsecutiveOnes_Solutions.cs
+++ 485_Max_Consecutive_Ones/MaxConsecutiveOnes_Solutions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// TODO Add Unit-tests
 namespace _485_Max_Consecutive_Ones
 {
     /// <summary>
