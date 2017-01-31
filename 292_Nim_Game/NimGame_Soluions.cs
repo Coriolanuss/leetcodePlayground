8fa28543250b40b0524c0f7c6ae753317f322964
Vadim <vadimpenchak@gmail.com>
2017-01-31 20:24:47 +0200

    Added data-driven unit-tests and ExpectedException tests. Refactored comment on CanWinNimMyFirstSolution().

--- 292_Nim_Game/NimGame_Soluions.cs
+++ 292_Nim_Game/NimGame_Soluions.cs
@@ -16,10 +16,12 @@ namespace _292_Nim_Game
         /// My first try to solve the problem without looking into any hints or references.
         /// </summary>
         /// <note>It turned out that this solution is best in terms math.</note>
-        /// <param name="n">Number of stones in the heap. Must be />0</param>
+        /// <param name="n">Number of stones in the heap. Must be &gt; 0 and &lt; Int32.MaxValue</param>
         /// <returns>"True" if you can win and "false" if you can't at the given conditions</returns>
         public static bool CanWinNimMyFirstSolution(int n)
         {
+            if (n <= 0)
+                throw new ArgumentOutOfRangeException("n", n, "Number of stones in the heap. Must be > 0 and < Int32.MaxValue");
             if (n % 4 == 0)
                 return false;
             else
---
+++ 292_Nim_GameTests/NimGame_SoluionsTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _292_Nim_Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _292_Nim_Game.Tests
+{
+    [TestClass()]
+    public class NimGame_SoluionsTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+            "TestData.xml",
+            "TestCase",
+            DataAccessMethod.Sequential)]
+        [TestMethod()]
+        public void CanWinNimMyFirstSolutionTest()
+        {
+            // Arrange
+            int numberOfStones = Convert.ToInt32(TestContext.DataRow["numb"]);
+            bool expected = Convert.ToBoolean(TestContext.DataRow["expected"]);
+
+            // Act
+            bool actual = NimGame_Soluions.CanWinNimMyFirstSolution(numberOfStones);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+            "ExceptionTestData.xml",
+            "TestCase",
+            DataAccessMethod.Sequential)]
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), AllowDerivedTypes = true)]
+        public void CanWinNimMyFirstSolutionTestExceptions()
+        {
+            // Arrange
+            int numberOfStones = Convert.ToInt32(TestContext.DataRow["numb"]);
+
+            // Act
+            NimGame_Soluions.CanWinNimMyFirstSolution(numberOfStones);
+        }
+    }
+}
